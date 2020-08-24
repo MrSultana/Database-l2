db0865d45fa21ab9ca2ba0a01600f93d2fbb3445
MrSultana <42900137+MrSultana@users.noreply.github.com>
2020-08-25 07:57:06 +1200

    first commit
    Cut all files into repository (except epub)

---
+++ 91892_Databases/91892_Support_Files/site_template/config.php
@@ -0,0 +1,8 @@
+<?php 
+    ini_set('display_errors', 'On'); //Change to off when live
+
+    define ("DB_NAME", "game_database");
+    define ("DB_USERNAME", "root");
+    define ("DB_PASSWORD", "usbw");
+    define_("DB_HOST", "localhost");
+?>
